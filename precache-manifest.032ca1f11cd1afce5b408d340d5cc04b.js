3ca35a65eda084535fc1c3de02444870cb0eccb9
AshwinM08 <ashwin.shenoy.m.8@gmail.com>
2019-08-04 17:20:42 +0530

    Updates

---
+++ precache-manifest.032ca1f11cd1afce5b408d340d5cc04b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "015f682be17774a62653",
+    "url": "/Ashwin_shenoy_resume/static/js/runtime~main.015f682b.js"
+  },
+  {
+    "revision": "b6935fa4f61fb801f5a1",
+    "url": "/Ashwin_shenoy_resume/static/js/main.b6935fa4.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Ashwin_shenoy_resume/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b6935fa4f61fb801f5a1",
+    "url": "/Ashwin_shenoy_resume/static/css/main.5611bbf7.chunk.css"
+  },
+  {
+    "revision": "8eedc067ed96fa1993284b67aa79e1c2",
+    "url": "/Ashwin_shenoy_resume/index.html"
+  }
+];
